bf01eb6b20f7a7478b997a0cc271c151b49b82bb
Nicholas Chen <nicholaschen@wifi-10-42-201-223.wifi.berkeley.edu>
2025-11-21 19:16:12 -0800

    Add Vercel adapter for SvelteKit deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		// Using Vercel adapter for deployment to Vercel platform
+		// Specify Node.js 20 runtime explicitly
+		adapter: adapter({
+			runtime: 'nodejs20.x'
+		})
 	}
 };
 
