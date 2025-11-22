949a4567b3bb8f0c9d2603a80dc8b25e11e4edc2
Nicholas Chen <nicholaschen@wifi-10-42-201-223.wifi.berkeley.edu>
2025-11-21 21:04:15 -0800

    Force bundling: vite in deps, external:[] config

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// Ensure @sveltejs/kit is bundled, not externalized
+			external: [],
+			isr: {
+				expiration: false
+			}
+		})
 	}
 };
 
